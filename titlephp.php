9f0344a1365467389b34c3dc6804e9d64efe7ee2
isaacsbm <isaacsonbm@gmail.com>
2024-06-02 22:52:58 -0700

    work please

--- titlephp.php
+++ titlephp.php
@@ -91,13 +91,11 @@
                                 // Create a PDO object
                                 $db = new PDO($dsn, $username, $password);
                                 // Prepare and execute the query
-                                $query = "INSERT INTO visitors (vistor_name) VALUES (visitor_name)";
-                                $query = "INSERT INTO visitors VALUES ('$visitor_name')";
+                                $query = "INSERT INTO visitors (vistor_name) VALUES (:visitor_name)";
                                 $stmt = $db->prepare($query);
                                 $stmt->bindParam(':visitor_name', $visitor_name);
                                 $stmt->execute();
-                                // Redirect to the about_us.html page
-                                header("Location: vistorlog.php");
+                                // Redirect to the about_us.html pages
                                 header("Location: visitorlog.php");
                                 exit();
                             }
