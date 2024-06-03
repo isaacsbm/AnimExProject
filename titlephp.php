bf305e7ff53dc3d1c077289f979fac5bbb97ddfe
isaacsbm <isaacsonbm@gmail.com>
2024-06-03 14:45:34 -0700

    did some minor cosmetics to the php page

--- titlephp.php
+++ titlephp.php
@@ -6,63 +6,60 @@
     <link rel="stylesheet" href="style.css">
     <title>Home Page</title>
     <style>
+    /* Popup */
+        .popup-container {
+            display: none;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 45%;
+            padding: 20px;
+            background: linear-gradient(to bottom right, #239EAB, #74DEEE) fixed;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+            border-radius: 10px;
+            z-index: 1000;
+        }
+        
+        .popup-form {
+            display: flex;
+            flex-direction: column;
+        }
+        
+        .popup-form h2, h3, label {
+            filter: drop-shadow(2px 2px 0px #000000);
+            text-shadow: none;
+        }
+        
+        .popup-form input{
+            margin-top: 10px;
+            font-size: 20px;
+            color: black;
+            padding: 10px;
+            border: 1px solid;
+            border-radius: 5px;
+        }
+        
+        .popup-form button {
+            margin-top: 10px;
+            padding: 10px;
+            background-color: #239EAB;
+            color: white;
+            border-radius: 5px;
+        }
+        
+        .popup-form button:hover {
+            background: linear-gradient(to bottom right, #239EAB, #74DEEE) fixed;
+        }
 
-/* Popup */
-.popup-container {
-    display: none;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 25%;
-    padding: 20px;
-    background-color: #74DFEE;;
-    
-    color: black;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    border-radius: 10px;
-    z-index: 1000;
-}
+        .popup-form h2 {
+            font-size: 50px;
+        }
+        .popup-container h3 {
+            font-size: 25px;
+        }
+            </style>
 
-.popup-form {
-    display: flex;
-    flex-direction: column;
-}
-
-.popup-form h2, h3, label {
-    color: black !important;
-    /* text-shadow: 2px 2px 2px black; */
-}
-
-.popup-form label {
-    margin-top: 10px;
-}
-
-.popup-form input{
-    margin-top: 5px;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-}
-
-.popup-form button {
-    margin-top: 10px;
-    padding: 10px;
-    background-color: #239EAB;
-    color: white;
-    border: none;
-    border-radius: 5px;
-}
-
-.popup-form button:hover {
-    background-color: #74DEEE;
-}
-
-.container:hover .popup-container {
-    display: block;
-}
-
-    </style>
 </head>
 <body>
     <div id="preloader">
